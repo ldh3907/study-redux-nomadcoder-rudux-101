6c6ce1378c8c32143faa037537cb32eadd3a5a5c
ldh3907 <ldh165163@gmail.com>
2021-08-06 22:55:33 +0900

    update : gitgrore, index
    toDo 50% 완성

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 import { createStore } from "redux";
 
-const plus = document.getElementById("plus")
-const minus = document.getElementById("minus")
-const number = document.querySelector("span")
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
 
-number.innerText= 0
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO= "DELETE_TODO"
 
-const countModifier = (count = 0, action) =>{
-  if(action.type === "add"){
-    return count +1;
-  }
-  else if(action.type ==="minus") {
-    return count -1;
-  }
-  else {
-    return count
-  }
+const reducer = (state=[], action) => {
+  switch(action.type){
+    case ADD_TODO:
+      return [ {text : action.text, id: Date.now()}, ...state,]
+      case DELETE_TODO:
+        return []
+        default:
+          return state
+        }
+      }
+const store = createStore(reducer)
+
+
+const paintToDos = () => {
+  const toDos = store.getState()
+  ul.innerHTML = ""
+  toDos.forEach(toDo => {
+    const li = document.createElement("li")
+    li.id = toDo.id
+    li.innerText = toDo.text
+    ul.appendChild(li)
+  })
+}
+store.subscribe(paintToDos)
+
+const addTodo = (text) =>{
+  store.dispatch({type : ADD_TODO, text})
 }
 
-const countStore = createStore(countModifier)
-
-const onChange = () =>{
-  number.innerText = countStore.getState()
-}
- 
-countStore.subscribe(onChange)
-
-const handleAdd = () =>{
-  countStore.dispatch({type : "add"})
+const onSubmit =(e) =>{
+  e.preventDefault()
+  const toDo = input.value
+  input.value = ""
+  addTodo(toDo)
 }
 
-const handleMinus = () =>{
-  countStore.dispatch({type : "minus"})
-}
-
-plus.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
-
+ form.addEventListener("submit", onSubmit)
