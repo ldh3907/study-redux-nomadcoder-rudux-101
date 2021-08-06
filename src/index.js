1eaeda6f3b7f674045d2408841bd281b732659de
ldh3907 <ldh165163@gmail.com>
2021-08-06 23:17:17 +0900

    update : index
    todo 80% 완성

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ const ul = document.querySelector("ul")
 const ADD_TODO = "ADD_TODO"
 const DELETE_TODO= "DELETE_TODO"
 
+const addToDo = text => {
+  return{type : ADD_TODO, text}
+}
+
+const deleteTodo = id => {
+  return {type: DELETE_TODO, id}
+}
+
 const reducer = (state=[], action) => {
   switch(action.type){
     case ADD_TODO:
@@ -19,28 +27,37 @@ const reducer = (state=[], action) => {
       }
 const store = createStore(reducer)
 
+const dispatchDeleteTodo = (e) =>{
+  const id= e.target.parentNode.id
+  store.dispatch(deleteTodo(id))
+}
+
+const dispatchAddTodo = (text = "내용없음") =>{
+  store.dispatch(addToDo(text))
+}
 
 const paintToDos = () => {
   const toDos = store.getState()
   ul.innerHTML = ""
   toDos.forEach(toDo => {
     const li = document.createElement("li")
+    const btn = document.createElement("button")
+    btn.innerText = "삭제"
+    btn.addEventListener("click", dispatchDeleteTodo)
     li.id = toDo.id
     li.innerText = toDo.text
+    li.appendChild(btn)
     ul.appendChild(li)
   })
 }
 store.subscribe(paintToDos)
 
-const addTodo = (text) =>{
-  store.dispatch({type : ADD_TODO, text})
-}
 
 const onSubmit =(e) =>{
   e.preventDefault()
   const toDo = input.value
   input.value = ""
-  addTodo(toDo)
+  dispatchAddTodo(toDo)
 }
 
  form.addEventListener("submit", onSubmit)
